7faacf249682031575bcbed3e855a85af85a6e53
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2024-01-21 20:58:41 -0300

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,16 +1,13 @@
 import { CalendarDemo } from "./Calendar-Date";
 import { ModeToggle } from "./ModeToggle";
 
-
-const SideBar= (): JSX.Element => {
+const SideBar = (): JSX.Element => {
   return (
     <div className="prose dark:prose-invert border-r w-[300px] py-10 px-5 flex flex-col justify-between  h-screen fixed">
       <h2>Agenda de demarcações</h2>
-      <div className="flex items-center justify-center">
-        <ModeToggle />
-      </div>
+      <ModeToggle />
     </div>
   );
-}
+};
 
 export default SideBar;
