e0135ad83716d0412ffaabcf3b3471c06d7e802b
KARAUJO1003 <129511975+KARAUJO1003@users.noreply.github.com>
2024-01-22 15:37:25 -0300

    Pagina de loadding

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,21 @@
+'use client'
+import { ThreeDots } from 'react-loader-spinner'
+
 const loading = () => {
   return (
-    <div className="flex items-center justify-center">
-      Carregando
+    <div className="h-screen w-full flex items-center justify-center">
+      <ThreeDots
+        visible={true}
+        height="80"
+        width="80"
+        color="#4fa94d"
+        radius="9"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
     </div>
   );
-}
+};
 
 export default loading;
--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import LogoDark02 from "@/assets/logo02dark.png";
+import LogoDark03 from "@/assets/logo03dark.png";
+import LogoLight03 from "@/assets/logo03light.png";
+import Link from "next/link";
+import LoginPage from "./login/page";
+import { useTheme } from "next-themes";
+import { UseThemeProps } from "next-themes/dist/types";
+
 export default function PortalPage() {
+
+
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      Enter
+      <Link href="/">
+        <div className="w-[400px]  overflow-hidden flex flex-col items-center justify-center">
+          <Image
+            loading="eager"
+            alt="logo"
+            src={ LogoDark03}
+            height={100}
+            width={300}
+            />
+        </div>
+      </Link>
     </div>
   );
 }
