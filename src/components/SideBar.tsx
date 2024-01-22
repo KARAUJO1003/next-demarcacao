60790f5260b2c0741206c5457b1e0defc20cbe87
KARAUJO1003 <129511975+KARAUJO1003@users.noreply.github.com>
2024-01-22 08:18:36 -0300

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,11 +6,13 @@ const SideBar = (): JSX.Element => {
   return (
     <div className="prose dark:prose-invert border-r w-[300px] py-10 px-5 flex flex-col justify-between  h-screen fixed">
       <h2>Agenda de demarcações</h2>
-      <ModeToggle />
+      <div className="flex items-center justify-between">
+        <ModeToggle />
 
-      <Link href="/portal/login">Login</Link>
-      <Link href="/portal/cadastro">Cadastro</Link>
-      <Link href="/portal">Portal</Link>
+        <Link href="/portal/login">Login</Link>
+        <Link href="/portal/cadastro">Cadastro</Link>
+        <Link href="/portal">Portal</Link>
+      </div>
     </div>
   );
 };
