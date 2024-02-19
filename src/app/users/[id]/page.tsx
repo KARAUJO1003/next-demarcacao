cfe4a9fba48f6c91649b9079bde55bbd4df81eea
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2024-02-19 14:49:00 -0300

    Pagina de detalhes do usuário.

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,22 +1,50 @@
 'use client'
 import { useEffect, useState } from "react";
 
-export default function PageUser({ params }: { params: { id: string } }) {
+interface User {
+  id: string;
+  name: string;
+  // Adicione outras propriedades conforme necessário
+}
+
+async function getData(): Promise<User[]> {
+  const res = await fetch('/api/users');
+  if (!res.ok) {
+    // Isso ativará o Error Boundary mais próximo em 'error.js'
+    throw new Error('Falha ao buscar dados');
+  }
+  const rawData = await res.json();
+  console.log("Dados brutos da API:", rawData);
   
-  const [users, setUsers] = useState([]);
+  // Verifique se a propriedade 'users' existe e é uma matriz
+  if (!Array.isArray(rawData.users)) {
+    throw new Error('Dados retornados não contêm uma matriz de usuários');
+  }
+  
+  return rawData.users;
+}
+
+export default function PageUser({ params }: { params: { id: string } }) {
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    fetch(`/api/users/${params.id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers(data.users);
-      });
+    const fetchData = async () => {
+      try {
+        const data = await getData();
+        setUsers(data);
+      } catch (error) {
+        console.error('Erro ao obter dados:', error);
+      }
+    };
+    fetchData();
   }, []);
 
-  return <>
-  <h1>My Page: {params.id}</h1>
-  {users.map((user)=>(
-
-  <h2>{user}</h2>
-  ))}
-  </>
+  return (
+    <>
+      <h1>Codigo: {params.id}</h1>
+      {users.map((item, index) => (
+        <span key={index}> Nome: {item.name}</span>
+      ))}
+    </>
+  );
 }
