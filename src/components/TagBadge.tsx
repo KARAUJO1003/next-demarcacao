f5ddc8342131764ce481d505c0891edee81a7d85
Kaesyo Félix <129511975+KARAUJO1003@users.noreply.github.com>
2024-02-26 09:36:10 -0300

    Alterando cores da tag

--- src/components/TagBadge.tsx
+++ src/components/TagBadge.tsx
@@ -10,7 +10,7 @@ interface TagBadgeProps {
 export function TagBadge({filtertag, nometag}:TagBadgeProps) {
   return (
     <div>
-      <Badge variant={"outline"}>
+      <Badge variant={"outline"} className={`${filtertag === 'Agendado' ? ' dark:bg-orange-950/60 dark:text-amber-500 ' :'dark:bg-green-950/60 dark:text-emerald-500'  }`}>
         {filtertag === "Agendado" ? (
           <MinusCircle className="mr-2 text-amber-600" size={11} />
         ) : (
